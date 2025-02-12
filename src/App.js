0f1e313d3fc93a6a2c1a3ffbd70943096dfc16b5
charan11w <charan11w@gmail.com>
2025-02-12 17:51:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from 'react';
+import Login from './components/pages/Login';
+import Home from './components/pages/Home'
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import './styles/main.css'
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  return (
+      <BrowserRouter>
+        {isAuthenticated && <Header />}
+        <Routes>
+          <Route path='/' element={isAuthenticated ? <Navigate to='/home' /> : <Login onLogin={handleLogin} />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+        {isAuthenticated && <Footer />}
+      </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+function Home() {
+  return (
+    <div>
+      <div className="home-page">
+        <div className="container">
+          hello
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default Home;
