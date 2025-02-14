0eb12f5d0dc76ff26d871eb47dba37dfe614f200
charan11w <charan11w@gmail.com>
2025-02-14 22:20:38 +0530

    added  some elments to the orders page

--- src/components/pages/Orders.js
+++ src/components/pages/Orders.js
@@ -1,6 +1,10 @@
+import emptyOrder from '../../images/icons/empty.png'
 function Orders() {
   return (
-    <div>orders</div>
+    <div className="order-container">
+        <h2 className='empty-cart'>Oops! Cart is Empty</h2>
+        <img src={emptyOrder} alt='ordersEmpty' className='ordersEmpty'></img>
+    </div>
   );
 }
 
