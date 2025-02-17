44a4ac88fe9930b60b81ffbe69076b1e50449af0
charan11w <charan11w@gmail.com>
2025-02-17 21:52:16 +0530

    changing restaurant pages

--- src/components/common/Restaurants.js
+++ src/components/common/Restaurants.js
@@ -1,21 +1,25 @@
-const restaurantList=[{
-  name:'Five Star Chicken',
-  rating:3.8,
-  address:"New gudupalli link road, near Babu nagar, Kuppam, Andhra Pradesh 517425",
-  delivaryTime:`${20}-${40} min`,
-  reviews:17,
-  positiveReview:`${72}%`,
+const restaurantList = [{
+  name: 'Five Star Chicken',
+  rating: 3.8,
+  address: "New gudupalli link road, near Babu nagar, Kuppam, Andhra Pradesh 517425",
+  delivaryTime: `${20}-${40} min`,
+  reviews: 17,
+  positiveReview: `${72}%`,
 
 }]
 
 function Restaurants() {
 
   return (
-    <div className="restarent">
-      <div className="container rant-cont">
-        <h3>Popular Stores</h3>
+    <div className="total-respage">
+      <div className="restaurant-page">
+        <div className=" popular-res">
+          <h2 className="popular">Popular Stores</h2>
+        </div>
       </div>
     </div>
+
+
   )
 }
 
