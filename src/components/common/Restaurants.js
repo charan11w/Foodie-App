de7aba3e123ce77870c3a91bf6ae076c53d8fb1a
charan11w <charan11w@gmail.com>
2025-02-27 19:15:25 +0530

    adding restaurants

--- src/components/common/Restaurants.js
+++ src/components/common/Restaurants.js
@@ -5,8 +5,16 @@ const restaurantList = [{
   delivaryTime: `${20}-${40} min`,
   reviews: 17,
   positiveReview: `${72}%`,
+  image: '../../images/nimage.png'
 }]
 
+
+const styles = {
+  image: {
+    width: '258.4px',
+    height: '130px'
+  }
+}
 
 
 function Restaurants() {
@@ -22,7 +30,16 @@ function Restaurants() {
             <button className="selection3">Non-Veg</button>
           </div>
           <div className="cord">
-
+            <div className="row">
+              <div className="col-3">
+                <div className="final-cord">
+                  <img src={restaurantList[0].image} style={styles.image} />
+                  <h3>{restaurantList[0].name}</h3>
+                  <div>{restaurantList[0].address}</div>
+                  <div>{restaurantList[0].delivaryTime}</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
