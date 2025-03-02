ba7676dfe1be111777f85eb9726210fe8dc87d84
charan11w <charan11w@gmail.com>
2025-03-02 22:47:23 +0530

    tried adding feature

--- src/components/pages/Restaurants.js
+++ src/components/pages/Restaurants.js
@@ -1,21 +1,32 @@
 import { useState } from "react";
-import RestaurantImage from '../../images/nimage.png'
-function Restaurants() {
+import RestaurantImage from "../../images/nimage.png";
 
-  const [activeFood, setActiveFood] = useState(0)
-  const showTypes = [{
-    type: 'all-btn',
-    name: 'All'
-  },
-  {
-    type: 'veg-btn',
-    name: 'Veg'
-  },
-  {
-    type: 'non-btn',
-    name: 'Non-Veg'
-  }
+function Restaurants() {
+  // Define state for active category
+  const [activeFood, setActiveFood] = useState("all");
+
+  // Define food types
+  const showTypes = [
+    { type: "all", name: "All" },
+    { type: "veg", name: "Veg" },
+    { type: "non-veg", name: "Non-Veg" },
   ];
+
+  // Dummy restaurant data with type (veg / non-veg)
+  const restaurants = [
+    { id: 1, name: "Green Leaf", type: "veg" },
+    { id: 2, name: "BBQ Nation", type: "non-veg" },
+    { id: 3, name: "Veg Delight", type: "veg" },
+    { id: 4, name: "Meat Lovers", type: "non-veg" },
+    { id: 5, name: "Pure Veg Heaven", type: "veg" },
+    { id: 6, name: "Grill House", type: "non-veg" },
+  ];
+
+  // **Filter restaurants based on selected category**
+  const filteredRestaurants = restaurants.filter((restaurant) =>
+    activeFood === "all" ? true : restaurant.type === activeFood
+  );
+
   return (
     <div className="resPage">
       <div className="container mtl">
@@ -23,237 +34,43 @@ function Restaurants() {
           <h3>Popular Restaurants</h3>
           <div className="res-type">
             <div className="store-type">
-              {showTypes.map((type, index) => {
-                return (
-
-                  <button
-                    key={index}
-                    className={`${type.type} ${activeFood === index ? 'activeFood' : ''}`}
-                    onClick={() => setActiveFood(index)} // Set activeFood on click
-                  >
-                    {type.name}
-                  </button>
-                )
-              })}
+              {showTypes.map((type, index) => (
+                <button
+                  key={index}
+                  className={`${type.type} ${activeFood === type.type ? "activeFood" : ""}`}
+                  onClick={() => setActiveFood(type.type)} // Set the active category on click
+                >
+                  {type.name}
+                </button>
+              ))}
             </div>
+
+            {/* Display Filtered Restaurants */}
             <div className="types-R row">
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
+              {filteredRestaurants.map((restaurant) => (
+                <div className="eatPlace col-3" key={restaurant.id}>
+                  <div className="center-div">
+                    <div className="img-cont">
+                      <img src={RestaurantImage} className="image-res" alt="Restaurant" />
                     </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
+                    <div className="detail-res">
+                      <div className="res-name">{restaurant.name}</div>
+                      <div className="star-btn">5.1</div>
+                      <div className="res-details">
+                        <p className="adrs-res">palace road rs pet kuppam</p>
+                        <p className="delivery-time">20 - 30 min</p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="eatPlace col-3">
-                <div className="center-div">
-                  <div className="img-cont">
-                    <img src={RestaurantImage} className="image-res" />
-                  </div>
-                  <div className="detail-res">
-                    <div className="res-name">
-                      Bangarpet Panipuri& Chaat
-                    </div>
-                    <div className="star-btn">5.1</div>
-                    <div className="res-details">
-                      <p className="adrs-res">
-                        palace road rs pet kuppam
-                      </p>
-                      <p className="delivery-time">
-                        20 - 30 min
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
-          </div>
 
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Restaurants;
