91fb46a6b9ef886b78cba8785d37fd29afcc43ce
charan11w <charan11w@gmail.com>
2025-04-07 16:47:57 +0530

    working in items age

--- src/components/ReusableComponents/Items.js
+++ src/components/ReusableComponents/Items.js
@@ -1,43 +1,61 @@
 import React from 'react'
 import '../../styles/items.css'
+import { FaSearch } from 'react-icons/fa';
 
 const Items = () => {
   return (
     <div className='container items-cont'>
       <div>
-        <div className='contanier'>
-          all products
-          <input type='checkbox' id="veg" />
-          <label for="veg" >Veg</label>
-          <input type='checkbox' id="non-veg" />
-          <label for="non-veg" >Non-Veg</label>
-          <input type='text' placeholder='Search for items...' />
-        </div>
-        <div className='all-items'>
-          <div>
-            <label for="pizza" >pizza</label>
-            <input type='checkbox' id="pizza" />
-            <label for="chats" >Chats & Streetfood</label>
-            <input type='checkbox' id="chats" />
-            <label for="burger" >burger</label>
-            <input type='checkbox' id="burger" />
-            <label for="chicken" >kabab & chicken</label>
-            <input type='checkbox' id="chicken" />
+        <div className='header-item'>
+          <div className='left'>All products</div>
+
+          <div className='center'>
+            <div className="filter">
+              <input type='checkbox' id="veg" />
+              <label htmlFor="veg">Veg</label>
+            </div>
+            <div className="filter">
+              <input type='checkbox' id="non-veg" />
+              <label htmlFor="non-veg">Non-Veg</label>
+            </div>
           </div>
-          <div>
+
+          <div className='right'>
+            <input type='text' placeholder='Search for items...' />
+            <FaSearch className="search-icon" />
+          </div>
+        </div>
+
+        <div className='all-items'>
+          <div className='filters'>
+            <div className='filter-option'>
+              <input type='checkbox' id="pizza" />
+              <label htmlFor="pizza">Pizza</label>
+            </div>
+            <div className='filter-option'>
+              <input type='checkbox' id="chats" />
+              <label htmlFor="chats">Chats & Streetfood</label>
+            </div>
+            <div className='filter-option'>
+              <input type='checkbox' id="burger" />
+              <label htmlFor="burger">Burger</label>
+            </div>
+            <div className='filter-option'>
+              <input type='checkbox' id="chicken" />
+              <label htmlFor="chicken">Kabab & Chicken</label>
+            </div>
+          </div>
+
+          <div className='items'>
             <div className='row'>
-              <div className='col-4'>
-                item
-              </div>
-              <div className='col-4'>
-                item
-              </div>
-              <div className='col-4'>
-                item
-              </div>
+              <div className='col'>item</div>
+              <div className='col'>item</div>
+              <div className='col'>item</div>
+              {/* Add more items as needed */}
             </div>
           </div>
         </div>
+
       </div>
     </div>
   )
